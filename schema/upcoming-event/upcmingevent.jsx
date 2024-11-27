9c5b97ee6af47f2219a5351ecb4bfcabcb6cb51e
Abhi Patel <ajink3994@gmail.com>
2024-11-27 18:49:17 +0530

    issue fixes

---
+++ schema/upcoming-event/upcmingevent.jsx
@@ -0,0 +1,21 @@
+import * as Yup from 'yup';
+
+export const upcomingListSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  date: Yup.string().required('Date is required'),
+  time: Yup.string().required('Time is required'),
+
+  description: Yup.string().required('Description is required'),
+
+  status: Yup.object({
+    label: Yup.string(),
+    value: Yup.string(),
+  }).required('Status is required'),
+  photo: Yup.mixed()
+    .nullable()
+    .test(
+      'fileType',
+      'Invalid file type',
+      (value) => !value || value instanceof File,
+    ),
+});
