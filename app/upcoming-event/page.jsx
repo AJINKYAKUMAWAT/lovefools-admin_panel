cf7bd94df583b22ad7ace3553f5589a34c82e9fe
Ajinkya Kumawat <ajink3994@gmail.com>
2024-12-02 21:56:31 +0530

    issue fixes

--- app/upcoming-event/page.jsx
+++ app/upcoming-event/page.jsx
@@ -241,7 +241,7 @@ const UpcomingEventList = () => {
                 <TableCell>{row.description}</TableCell>
 
                 <TableCell>
-                  <Image
+                  <img
                     height={10}
                     width={70}
                     style={{ maxHeight: '50px' }}
